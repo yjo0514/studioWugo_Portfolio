3f51bfc02bb591a2113da226fbd9e6032bcd5d32
yjo0514 <yjo0514@naver.com>
2021-10-20 18:01:15 +0900

    studioWugo_Portfolio

--- script/script.js
+++ script/script.js
@@ -4,20 +4,33 @@ $(function(){
 
      let leftValue="0";
 
-     $("#gnbBtn>a").click(function(){
-     $("#sideMenuArea").stop().animate({"left": leftValue},
-        800,
-        function(){
-            if(leftValue=="0"){
-                leftValue="-320px";
-            }else if(leftValue=="-320px"){
-                leftValue="0";
-            }
-        }
-        );
+     let chkToF = true;
+     $("#gnbBtn a").click(function() {
+         $(this).toggleClass("applied");
+ 
+         $("#sideMenuBG").fadeToggle(250);
+         if (chkToF) {
+              $("#sideMenuArea").animate({"left": "0px"}, 250);
+              chkToF = false;
+             } else {
+             $("#sideMenuArea").animate({"left": "-320px"}, 250);
+             chkToF = true;
+             }
+     });
 
-    });
-    
+     $("div#sideMenuArea button").click(function(){
+        $(this).toggleClass("applied");
+ 
+        $("#sideMenuBG").fadeToggle(250);
+        if (chkToF) {
+             $("#sideMenuArea").animate({"left": "0px"}, 250);
+             chkToF = false;
+            } else {
+            $("#sideMenuArea").animate({"left": "-320px"}, 250);
+            chkToF = true;
+            }
+     });
+
      //sideMenu 끝
 
 
